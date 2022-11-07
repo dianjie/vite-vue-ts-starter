6e76863af4331e6449c2b4a488a6d81708308097
xiaoyu <dianjie1022@gmail.com>
2022-11-07 14:01:46 +0800

    fix: button style influence

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  corePlugins: {
+    // https://github.com/tailwindlabs/tailwindcss/discussions/5969
+    preflight: false
+  },
   theme: {
     extend: {}
   },
